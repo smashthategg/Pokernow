175fc3f7b4afd416cd6ceea660e801c38453f9b7
Johnny <johnnylin310@gmail.com>
2024-01-20 01:16:04 -0800

    lowkey broke everything but redoing code to add more functionality!

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+import os
 from sqlbuilder import build_sql_database_framework
 from reader import csv_to_df
-import os
 
-player = "smashthategg"
-
-print(csv_to_df(player))
+print(csv_to_df())
 
 if not os.path.exists("./pokernow_sqlite.db"):
     build_sql_database_framework()
--- reader.py
+++ reader.py
@@ -2,16 +2,43 @@ import os
 import re
 import pandas as pd
 
-# Iterate through each pokernow.club csv file in game_logs
-def csv_to_df(player):
+'''
+                    !!! IMPORTANT NOTES !!!
+    If a csv file is downloaded from a game played by someone else,
+    it must be edited at the bottom to include that player's exact username.
+    This will change the "you" player to them, which is necessary because
+    otherwise the program will not function properly :)
 
-    games = [] # Will compile the data frames from each file into this list before concatenating them all
-    parsed_files_file = open("./parsed_files.txt", "w")
-    parsed_files = [] # Will also store the filenames so we know which ones have already been parsed
+    Also, hands played without the "you" player 
+    (after they go broke) will not be recorded.
+ '''
+
+# Iterate through each pokernow.club csv file in game_logs
+def csv_to_df():
+
+    # !!! IMPORTANT !!! This is the "you" player by default. CSV files from this player need not be edited
+    you = "smashthategg" 
+
+    hand_id = 0
+    positions = ['UTG','UTG+1','LJ','HJ','CO','BTN','SB','BB']
+    players = {} 
+    # Will be the single storage of all csv data, a massive dictionary with key-value pairs string and another dictionary
+    '''
+    { 
+        name : {
+                'hand_ids' : []     list of integers
+                'stacks' : []             ^^^
+                'stacksinbbs' : []        ^^^
+                'blinds' : []       list of ordered pair of integers (sb, bb)
+                'hands' : []        list of strings either the player's hand or 'none' if it was never revealed
+                'positions': []     list of strings taken from the list 'positions'
+                'action' : []       list of strings
+               }
+    }
+    '''
 
     for fileName in os.listdir('./game_logs'): 
         logs = pd.read_csv('./game_logs/{}'.format(fileName))
-        parsed_files.append('{}\n'.format(fileName))
         
         # Put each row entry into an array
         entries = []
@@ -23,85 +50,79 @@ def csv_to_df(player):
 
         entries = entries[::-1] # Reverse the order of the array (to Oldest -> Newest entry)
 
-        # Lists to store various data
-        btn_players = []
-        players = []
-        mystacksbb = []
-        mystacks = []
-        stacks = []
-        bbs = []
-        hands = []
-        blinds = []
-        
-        # Keep track of target player, only read hands with player in table.
-        playerQuit = False
-        my_player_index = 0
-        index = 0
-        stacks_index = 0
+        # If the file is from a game played by someone else, the script needs to know that.
+        if len(entries[0]) < 20:
+            you = entries[0]
 
         # Now to iterate through the log line by line
-        while index < len(entries) and not playerQuit:
+        index = 0
+        while index < len(entries):
 
             # First line will always be "Starting hand ... Button is (some_player)" (may not be necessary to record)
+            '''
             btn_player = re.search(r'\(de.*?"', entries[index])
             if btn_player:
                 btn_players.append(btn_player.group()[11:-1])
             else:
                 btn_players.append('*dead button*')
             index += 1
+            '''
 
-            # Skip occasional filler lines
+            # Skip filler lines
             while entries[index][0] != 'P':
                 index += 1
 
-            # Record all current players and stacks in table
-            players.append(re.findall(r'"(.*?)"', entries[index]))
-            stacks.append(re.findall(r'\((.*?)\)', entries[index]))
-            stacks[stacks_index] = list(map(int, stacks[stacks_index]))
-
-            # Get target player-specific info (hand and position)
-            my_player_index = players[stacks_index].index(player)
+            players_in_hand = re.findall(r'"(.*?)"', entries[index])
+            if you not in players_in_hand:
+                break
+            stacks = re.findall(r'\((.*?)\)', entries[index])
+            for name in players_in_hand:
+                if name not in players:
+                    # Initialize the key-value pair in players if player has not been added yet.
+                    players[name] = {
+                                     'hand_ids' : [],
+                                     'hands': [],
+                                     'stacks': [],
+                                     'stacksinbbs': [],
+                                     'blinds': [],
+                                     'positions': [],
+                                     'action': []
+                                    }
+                # Record the player's current stack and hand_id
+                players[name]['stacks'].append(int(stacks[players_in_hand.index(name)]))
+                players[name]['hand_ids'].append(hand_id)
             index += 1
-            hands.append(entries[index][13:])
+            
+            # Record the "you" player's hand
+            players[you]['hands'].append(entries[index][13:])
             index += 1
 
             # Record the current blind level
             if 'posts a big blind' in entries[index]:
-                blinds.append(int(entries[index].split()[-1]))
+                bb = int(entries[index].split()[-1])
             else:
                 index += 1
-                blinds.append(int(entries[index].split()[-1]))
+                bb = int(entries[index].split()[-1])
+            for name in players_in_hand:
+                players[name]['blinds'].append((bb//2, bb))
+            index += 1
 
-            # Divide all stacks by bb size to get stacks in bbs. Also record target player's stack
-            bbs.append([round(x / blinds[stacks_index], 1) for x in stacks[stacks_index]])
-            mystacks.append(stacks[stacks_index][my_player_index])
-            mystacksbb.append(bbs[stacks_index][my_player_index])
-            stacks_index += 1
+            # Divide all stacks by bb size to get stacks in bbs.
+            for name in players_in_hand:
+                players[name]['stacksinbbs'].append(round(players[name]['stacks'][-1] / bb, 1))
             
+            # Record each player's position
+            if index + 7 < len(entries):
+                for i in range(8-len(players_in_hand),8):
+                    curr_player = re.search(r'"(.*?)"', entries[index]).group(1)
+                    print(entries[index])
+                    players[curr_player]['positions'].append(positions[i])
+                    index += 1
+
             # Skip over lines until reader arrives at next hand. Also quits reading if target player is no longer in game.
             while index < len(entries) and entries[index][:4] != '-- s':
-                if 'The player "{}" quits'.format(player) in entries[index]:
-                    playerQuit = True
-                    break
                 index += 1
 
-        # Create data frame for current file
-        data =  {
-                    'Hands': hands,
-                    'My Stack Size': mystacks,
-                    'My Stack Size (BBs)': mystacksbb,
-                    'Players': players,
-                    'Stacks (BBs)': bbs
-                }
-    
-    
-    parsed_files_file.writelines(parsed_files)
-    parsed_files_file.close()
-    games.append(pd.DataFrame(data))
-    return(pd.concat(games))
+            hand_id += 1
 
-
-
-
-
-
+    print(players[you]['positions'])
