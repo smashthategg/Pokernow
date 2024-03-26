598e070e8163c90683084685b7975373665d6eeb
TOXICXUHUAN <58757783+TOXICXUHUAN@users.noreply.github.com>
2024-03-25 21:19:08 -0700

    player.py template

---
+++ poker_simulator/player.py
@@ -0,0 +1,18 @@
+from deck import Deck
+
+class Player:
+  def __init__(self, name, chips):
+    self.name = name
+    self.hand = []
+    self.chips = chips
+
+  def deal(self, cards):
+    self.hand = cards
+
+  def bet(self, amount):
+    if amount <= self.chips:
+      self.chips -= amount
+      return True
+    else:
+      print(f"{self.name} doesn't have enough chips to bet {amount}.")
+      return False
