7bd6e8dc84d229b299f03381a51d6ef744ba9fd8
Xinlong Yang <xinlong@ucsb.edu>
2024-04-14 18:08:56 -0700

    stats.py

--- stats.py
+++ stats.py
@@ -10,20 +10,23 @@ for fileName in os.listdir('./game_logs'):
 
 dict = r.players
 def calculate_stats(dict):
-    #create an empty dictionary for all the data
+    # create an empty dictionary for all the data
     df = {}
     for players in dict:
-        VPIP = sum(map(lambda x: x != 'X' and x != 'F', dict[players]['preflop']))/ len(dict[players]['preflop'])
+        VPIP = sum(map(lambda x: x != 'X' and x != 'F', dict[players]['preflop'])) / len(dict[players]['preflop'])
+        # VPIP: Using map function to remove checks and folds
         SD = sum(dict[players]['showdown'])/len(dict[players]['showdown'])
+        # showdown true / all games
         WSD = 0
         WTSD = 0
         for i in range(0, len(dict[players]['stack']) - 2):
             if dict[players]['showdown'][i]:
                 WSD += (dict[players]['stack'][i+1] - dict[players]['stack'][i])
+                # gains 
             else:
                 WTSD += (dict[players]['stack'][i+1] - dict[players]['stack'][i]) 
-        df.update({players: ['VPIP: ' + str(VPIP), 'SD: ' + str(SD), 
-                             'WSD: ' + str(WSD), 'WTSD: ' + str(WTSD)]})
+        df.update({players: {'VPIP': VPIP, 'SD': SD, 
+                             'WSD': WSD, 'WTSD': WTSD}})
     
     return df
 # VPIP: Voluntarily put into pot        
@@ -32,4 +35,8 @@ def calculate_stats(dict):
 # WTSD: total won/lost without showdown
 
 df = calculate_stats(dict)
-print(df.get('smashthategg'))
+# print(df.get('smashthategg')) # use this format to get a specific player
+stats = json.dumps(df, indent = 4) # use this format to get ALL players
+print(stats)
+with open('stats', 'w') as player_stats:
+    json.dump(df, player_stats)
